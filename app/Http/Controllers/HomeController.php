893da292530e6c628f8d66199e7ddb5f1d3a6f1f
Bryleva Elena <kitsune94@mail.ru>
2018-09-13 00:41:22 +0700

    Правки

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,8 +27,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $lastPastes = $this->getLastPastes();
+        $lastPastes = $pastes = DB::table('pastes')
+            ->latest()
+            ->where(function($query){
+                $query->where('access_all', '=', true)
+                ->orWhere('life_time', '>', date("Y-m-d H:i:s"));
+            })
+            ->where('private', '=', '0')
+            ->limit(10)
+            ->get();
+            
         $userPastes = DB::table('pastes')
+            ->latest()
+            ->where('user_id', '=', Auth::user()->id)
+            ->where(function($query){
+                $query->where('access_all', '=', true)
+                ->orWhere('life_time', '>', date("Y-m-d H:i:s"));
+            })
+            ->limit(10)
+            ->get();
+            
+        $userPastesTable = DB::table('pastes')
             ->where('user_id', '=', Auth::user()->id)
             ->where(function($query){
                 $query->where('access_all', '=', true)
@@ -38,33 +57,8 @@ class HomeController extends Controller
 
         return view('home')->with([
                             'userPastes' => $userPastes,
+                            'userPastesTable' => $userPastesTable,
                             'lastPastes' => $lastPastes
-                          ]);;
-    }
-    
-    private function getLastPastes(){
-        $pastes = DB::table('pastes')
-            ->latest()
-            ->where(function($query){
-                $query->where('access_all', '=', true)
-                ->orWhere('life_time', '>', date("Y-m-d H:i:s"));
-            })
-            ->where('private', '=', '0')
-            ->limit(10)
-            ->get();
-            return $pastes;
-    }
-    
-    private function getLastUserPastes(){
-        $userPastes = DB::table('pastes')
-            ->latest()
-            ->where('user_id', '=', Auth::user()->id)
-            ->where(function($query){
-                $query->where('access_all', '=', true)
-                ->orWhere('life_time', '>', date("Y-m-d H:i:s"));
-            })
-            ->limit(10)
-            ->get();
-            return $userPastes;
+                          ]);
     }
 }
